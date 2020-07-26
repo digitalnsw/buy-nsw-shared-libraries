85c87049849f5fa98ef38ea61336022ee69abe7a
Arman <arman.zrb@gmail.com>
2020-07-26 20:44:45 +1000

    require new classes

--- lib/shared_modules.rb
+++ lib/shared_modules.rb
@@ -2,4 +2,7 @@ module SharedModules
   require 'shared_modules/authentication'
   require 'shared_modules/serializer'
   require 'shared_modules/session_user'
+  require 'shared_modules/application_job'
+  require 'shared_modules/slack_post_job'
+  require 'shared_modules/slack_reporting_job'
 end
