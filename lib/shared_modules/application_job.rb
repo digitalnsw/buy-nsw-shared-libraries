aefdcdb0212d1fda89ad726d3d4ce27d1b56ea6b
Arman <arman.zrb@gmail.com>
2020-08-02 13:49:12 +1000

    Using default-queue-name env var

--- lib/shared_modules/application_job.rb
+++ lib/shared_modules/application_job.rb
@@ -1,7 +1,7 @@
 module SharedModules
   class ApplicationJob < ActiveJob::Base
     # At the moment everything gets queued in the same queue
-    queue_as ENV.fetch('MAILER_QUEUE_NAME', :default)
+    queue_as ENV.fetch('DEFAULT_QUEUE_NAME', :default)
 
     rescue_from Exception do |exception|
       Airbrake.notify_sync exception
