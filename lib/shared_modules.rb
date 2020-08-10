c00dd72d94364f2eb46f745ceb66bd8005666cdd
Arman <arman.zrb@gmail.com>
2020-08-10 16:35:04 +1000

    Add json validator

--- lib/shared_modules.rb
+++ lib/shared_modules.rb
@@ -2,6 +2,7 @@ module SharedModules
   require 'shared_modules/authentication'
   require 'shared_modules/serializer'
   require 'shared_modules/session_user'
+  require 'shared_modules/json_validator'
   require 'shared_modules/application_job'
   require 'shared_modules/slack_post_job'
   require 'shared_modules/slack_reporting_job'
