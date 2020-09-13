9cce94b5cc91ee48b85d44494efb77f30be53278
Arman <arman.zrb@gmail.com>
2020-09-13 12:45:35 +1000

    minor fix for multi owner

--- lib/shared_resources/remote_waiting_seller.rb
+++ lib/shared_resources/remote_waiting_seller.rb
@@ -10,8 +10,8 @@ module SharedResources
       nil
     end
 
-    def self.initiate_seller(id, owner_id)
-      r = post "#{id}/initiate_seller", { owner_id: owner_id }
+    def self.initiate_seller(id)
+      r = post "#{id}/initiate_seller"
       JSON.parse(r.body)['seller_id']
     end
   end
