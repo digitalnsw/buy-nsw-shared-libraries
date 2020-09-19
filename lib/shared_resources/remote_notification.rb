ae8ced1ce96678ee9ec1696446cc346452948ccd
Arman Sarrafi <arman.zrb@gmail.com>
2020-09-19 11:08:06 +1000

    Add unifier to notifications

--- lib/shared_resources/remote_notification.rb
+++ lib/shared_resources/remote_notification.rb
@@ -4,8 +4,14 @@ module SharedResources
     self.element_name = "notification"
     self.generate_token
 
-    def self.create_notification(recipients:, subject:, body:, fa_icon: nil, actions: [])
-      create(recipients: recipients, subject: subject, body: body, fa_icon: fa_icon, actions: actions)
+    def self.create_notification(recipients:, subject:, body:, fa_icon: nil, actions: [], unifier: nil)
+      create(unifier: unifier, recipients: recipients, subject: subject, body: body, fa_icon: fa_icon, actions: actions)
+    rescue ActiveResource::ClientError => e
+      if e.message.match?(/\b406\b/)
+        raise SharedModules::AlertError.new('Request canceled! A duplicate request is pendinding in the queue.')
+      else
+        raise e.message
+      end
     end
   end
 end
