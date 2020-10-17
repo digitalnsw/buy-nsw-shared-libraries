ed69fd5a0e5aaef297cd68b05c859a9bb9b434d8
Arman Sarrafi <arman.zrb@gmail.com>
2020-10-18 10:14:38 +1100

    fix escape method name

--- lib/shared_modules/serializer.rb
+++ lib/shared_modules/serializer.rb
@@ -4,10 +4,21 @@ module SharedModules
   module Serializer
     include ERB::Util
 
-    def escape_recursive input
+    def unescape_recursive input
       if input.is_a? String
         CGI.unescapeHTML input
-        # TODO: html_escape_once input if exposed to third part that doesnt escape
+      elsif input.is_a? Array
+        input.map{|e| escape_recursive(e)}
+      elsif input.is_a? Hash
+        input.map{|k,v| [escape_recursive(k), escape_recursive(v)]}.to_h
+      else
+        input
+      end
+    end
+
+    def escape_recursive input
+      if input.is_a? String
+        html_escape_once input
       elsif input.is_a? Array
         input.map{|e| escape_recursive(e)}
       elsif input.is_a? Hash
